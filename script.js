5f06e0f1549972969ad305a30813751406d7dd1e
Dylan Best <best.dylan15@gmail.com>
2020-07-17 14:45:12 -0400

    added answer button on click events

--- script.js
+++ script.js
@@ -1,125 +1,138 @@
-//main variable
-var main = $("#main");
+$(document).ready(function () {
 
-// timer variables
-var secondsLeft = 75;
-var timer = $("#timer").text(`Time: ${secondsLeft}`);
+    //main variable
+    var main = $("#main");
 
-// question and answer variables
-var question = "question";
-var answers = ["1", "2", "3", "4"];
-var questionIndex = 1;
+    // timer variables
+    var secondsLeft = 75;
+    var timer = $("#timer").text(`Time: ${secondsLeft}`);
 
-var question1 = {
-    question: "Commonly used data types DO NOT include:",
-    answer1: "1. strings",
-    answer2: "2. boleans",
-    answer3: "3. alerts",
-    answer4: "4. numbers"
-}
+    // question and answer variables
+    var question;
+    var answers = [];
+    var questionIndex = 1;
 
-var question2 = {
-    question: "The condition in an if/else statement is enclosed within _.",
-    answer1: "1. quotes",
-    answer2: "2. curly brackets",
-    answer3: "3. parentheses",
-    answer4: "4. square brackets"
-}
+    var question1 = {
+        question: "Commonly used data types DO NOT include:",
+        answer1: "1. strings",
+        answer2: "2. boleans",
+        answer3: "3. alerts",
+        answer4: "4. numbers"
+    }
 
-var question3 = {
-    question: "Arrays in Javascript can be used to store _.",
-    answer1: "1. numbers and strings",
-    answer2: "2. other arrays",
-    answer3: "3. booleans",
-    answer4: "4. all of the above"
-}
+    var question2 = {
+        question: "The condition in an if/else statement is enclosed within _.",
+        answer1: "1. quotes",
+        answer2: "2. curly brackets",
+        answer3: "3. parentheses",
+        answer4: "4. square brackets"
+    }
 
-var question4 = {
-    question: "String values must be enclosed within _ when being assigned to variables.",
-    answer1: "1. commas",
-    answer2: "2. curly brackets",
-    answer3: "3. quotes",
-    answer4: "4. parentheses"
-}
+    var question3 = {
+        question: "Arrays in Javascript can be used to store _.",
+        answer1: "1. numbers and strings",
+        answer2: "2. other arrays",
+        answer3: "3. booleans",
+        answer4: "4. all of the above"
+    }
 
-var question5 = {
-    question: "A very useful tool used during development and debugging for printing content to the debugger is:",
-    answer1: "1. Javascript",
-    answer2: "2. terminal / bash",
-    answer3: "3. for loops",
-    answer4: "4. console.log"
-}
+    var question4 = {
+        question: "String values must be enclosed within _ when being assigned to variables.",
+        answer1: "1. commas",
+        answer2: "2. curly brackets",
+        answer3: "3. quotes",
+        answer4: "4. parentheses"
+    }
 
-//starts timer
-function setTime() {
+    var question5 = {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        answer1: "1. Javascript",
+        answer2: "2. terminal / bash",
+        answer3: "3. for loops",
+        answer4: "4. console.log"
+    }
 
-    var timerInterval = setInterval(function () {
-        secondsLeft--;
-        timer.text(`Time: ${secondsLeft}`);
+    //starts timer
+    function setTime() {
 
-        if (secondsLeft === 0) {
-            clearInterval(timerInterval);
-            // run function for time up
+        var timerInterval = setInterval(function () {
+            secondsLeft--;
+            timer.text(`Time: ${secondsLeft}`);
+
+            if (secondsLeft === 0) {
+                clearInterval(timerInterval);
+                // run function for time up
+            }
+
+        }, 1000);
+    }
+
+    //sets up question and answer variables
+    function setQuestion() {
+        if (questionIndex === 1) {
+            question = question1.question;
+            answers = [question1.answer1, question1.answer2, question1.answer3, question1.answer4];
+        }
+        else if (questionIndex === 2) {
+            question = question2.question;
+            answers = [question2.answer1, question2.answer2, question2.answer3, question2.answer4];
+        }
+        else if (questionIndex === 3) {
+            question = question3.question;
+            answers = [question3.answer1, question3.answer2, question3.answer3, question3.answer4];
+        }
+        else if (questionIndex === 4) {
+            question = question4.question;
+            answers = [question4.answer1, question4.answer2, question4.answer3, question4.answer4];
+        }
+        else if (questionIndex === 5) {
+            question = question5.question;
+            answers = [question5.answer1, question5.answer2, question5.answer3, question5.answer4];
+        }
+        else {
+            //go to highscore screen
         }
 
-    }, 1000);
-}
+        //sets index to next question
+        questionIndex++;
+    }
 
-//sets up question and answer variables
-function setQuestion() {
-    if (questionIndex = 1) {
-        question = question1.question;
-        answers = [question1.answer1, question1.answer2, question1.answer3,  question1.answer4];
-    }
-    else if(questionIndex = 2) {
-        question = question2.question;
-        answers = [question2.answer1, question2.answer2, question2.answer3, question2.answer4];
-    }
-    else if(questionIndex = 3) {
-        question = question3.question;
-        answers = [question3.answer1, question3.answer2, question3.answer3, question3.answer4];
-    }
-    else if(questionIndex = 4) {
-        question = question4.question;
-        answers = [question4.answer1, question4.answer2, question4.answer3, question4.answer4];
-    }
-    else {
-        question = question5.question;
-        answers = [question5.answer1, question5.answer2, question5.answer3, question5.answer4];
-    }
-}
+    //creates next question with answers
+    function createQuestion() {
 
-//creates next question with answers
-function createQuestion() {
+        //creates new question text
+        var newQuestion = $("<h1>");
+        main.append(newQuestion);
+        newQuestion.text(question);
 
-    //creates new question text
-    var newQuestion = $("<h1>");
-    main.append(newQuestion);
-    newQuestion.text(question);
-
-    //creates new answer buttons
-    for (var i = 0; i < answers.length; i++) {
-        var newAnswer = $("<button>");
-        main.append(newAnswer);
-        newAnswer.attr("class", "btn btn-primary");
-        newAnswer.text(answers[i]);
-        main.append($("<br>"));
+        //creates new answer buttons
+        for (var i = 0; i < answers.length; i++) {
+            var newAnswer = $("<button>");
+            main.append(newAnswer);
+            newAnswer.attr("class", "btn btn-primary answerbutton");
+            newAnswer.text(answers[i]);
+            main.append($("<br>"));
+        }
     }
+
+    //empties the main text
+    function nextStage() {
+        main.empty();
+    }
+
+    //start quiz button
+    $("#start").on("click", function () {
+        setTime();
+        nextStage();
+        setQuestion();
+        createQuestion();
+    });
 
     //answer button click events
-    $("button.btn").on("click", function() {
+    $(document).on("click", "button.answerbutton", function () {
         nextStage();
-    })
-}
+        setQuestion();
+        createQuestion();
+    });
 
-//empties the main text
-function nextStage() {
-    main.empty();
-}
-
-//start quiz button
-$("#start").on("click", function() {
-    setTime();
-    nextStage();
-    createQuestion();
 });
