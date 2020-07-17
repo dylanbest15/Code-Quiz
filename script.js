daf5769a77fcb1899e61e5ac56da0d9c7078a193
Dylan Best <best.dylan15@gmail.com>
2020-07-17 14:07:41 -0400

    set up question and answer variables

--- script.js
+++ script.js
@@ -1,8 +1,56 @@
-// timer variables
-var timer = $("span.timer");
-var secondsLeft = 75;
+//main variable
+var main = $("#main");
 
-// starts timer
+// timer variables
+var secondsLeft = 75;
+var timer = $("#timer").text(`Time: ${secondsLeft}`);
+
+// question and answer variables
+var question = "question";
+var answers = ["1", "2", "3", "4"];
+var questionIndex = 1;
+
+var question1 = {
+    question: "",
+    answer1: "",
+    answer2: "",
+    answer3: "",
+    answer4: "",
+}
+
+var question2 = {
+    question: "",
+    answer1: "",
+    answer2: "",
+    answer3: "",
+    answer4: "",
+}
+
+var question3 = {
+    question: "",
+    answer1: "",
+    answer2: "",
+    answer3: "",
+    answer4: "",
+}
+
+var question4 = {
+    question: "",
+    answer1: "",
+    answer2: "",
+    answer3: "",
+    answer4: "",
+}
+
+var question5 = {
+    question: "",
+    answer1: "",
+    answer2: "",
+    answer3: "",
+    answer4: "",
+}
+
+//starts timer
 function setTime() {
 
     var timerInterval = setInterval(function () {
@@ -17,10 +65,61 @@ function setTime() {
     }, 1000);
 }
 
-// start quiz button
-$("button.btn-primary").on("click", function () {
-    setTime();
-});
+//sets up question and answer variables
+function setQuestion() {
+    if (questionIndex = 1) {
+        question = question1.question;
+        answers = [question1.answer1, question1.answer2, question1.answer3,  question1.answer4];
+    }
+    else if(questionIndex = 2) {
+        question = question2.question;
+        answers = [question2.answer1, question2.answer2, question2.answer3, question2.answer4];
+    }
+    else if(questionIndex = 3) {
+        question = question3.question;
+        answers = [question3.answer1, question3.answer2, question3.answer3, question3.answer4];
+    }
+    else if(questionIndex = 4) {
+        question = question4.question;
+        answers = [question4.answer1, question4.answer2, question4.answer3, question4.answer4];
+    }
+    else {
+        question = question5.question;
+        answers = [question5.answer1, question5.answer2, question5.answer3, question5.answer4];
+    }
+}
 
-//sets up initial timer
-timer.text(`Time: ${secondsLeft}`);
+//creates next question with answers
+function createQuestion() {
+
+    //creates new question text
+    var newQuestion = $("<h1>");
+    main.append(newQuestion);
+    newQuestion.text(question);
+
+    //creates new answer buttons
+    for (var i = 0; i < answers.length; i++) {
+        var newAnswer = $("<button>");
+        main.append(newAnswer);
+        newAnswer.attr("class", "btn btn-primary");
+        newAnswer.text(answers[i]);
+        main.append($("<br>"));
+    }
+
+    //answer button click events
+    $("button.btn").on("click", function() {
+        nextStage();
+    })
+}
+
+//empties the main text
+function nextStage() {
+    main.empty();
+}
+
+//start quiz button
+$("#start").on("click", function() {
+    setTime();
+    nextStage();
+    createQuestion();
+});
